a7dd01191f24bad7950ce71398dfc9cf07145fce
Eric Mignardi <mignardi.e@gmail.com>
2025-10-03 08:04:50 -0400

    Initial commit

---
+++ backend/src/main/java/com/fanimal/backend/FanimalApplication.java
@@ -0,0 +1,13 @@
+package com.fanimal.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanimalApplication.class, args);
+	}
+
+}
